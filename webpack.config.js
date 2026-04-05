7727c6f18363071fa5bf9ce8c7a4aeb511a38609
sovit <sovit2017nayak@gmail.com>
2026-04-04 21:59:47 -0400

    build: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'viz.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: 'manifest.json' },
+        { from: 'src/viz-config.json', to: 'viz-config.json' },
+        { from: 'src/viz.css', to: 'viz.css' },
+      ],
+    }),
+  ],
+};
